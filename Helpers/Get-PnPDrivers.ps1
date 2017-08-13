a028e8000ae274289c4fee97d0082d85f33be36c
richprescott <richprescott@users.noreply.github.com>
2017-08-13 18:03:06 -0400

    Initial upload

---
+++ Helpers/Get-PnPDrivers.ps1
@@ -0,0 +1,11 @@
+﻿function Get-PnPDrivers
+{
+    Param(
+        $Exclude = 'Generic USB|standard|Microsoft'
+    )
+
+    Get-WmiObject -Class Win32_PnPSignedDriver |
+    Where-Object {$_.InfName -and $_.Manufacturer -notmatch $Exclude} |
+    Sort-Object Manufacturer, DeviceName |
+    Select-Object Manufacturer, DeviceName, FriendlyName, InfName, IsSigned, DriverVersion
+}
